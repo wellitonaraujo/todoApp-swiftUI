e572d15fef4bd48096f9c63e517c3fc2a8406ee2
Welliton <welliton.araujo@compasso.com.br>
2022-10-26 11:57:40 -0300

    ïnit commit

---
+++ Notes App WatchKit Extension/Notes_AppApp.swift
@@ -0,0 +1,19 @@
+//
+//  Notes_AppApp.swift
+//  Notes App WatchKit Extension
+//
+//  Created by Welliton da Conceicao de Araujo on 26/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Notes_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
